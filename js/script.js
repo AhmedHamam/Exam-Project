53ae377e0c107258d74ce207b7bf12189ade5d0a
Mohammednayel93 <43174693+Mohammednayel93@users.noreply.github.com>
2019-04-26 04:29:02 +0200

    Login and Register Compeleted

--- js/script.js
+++ js/script.js
@@ -1,21 +1,111 @@
 $(document).ready(function() {
+    //   Login
+    var txt_EmailLogin = $('#emailLogin');
+    var txt_PasswordLogin = $('#passwordLogin');
+    var btnLogin = $('#btnLogin');
+    var showModelLogin = $('#showmodalLogin');
+    // end Login
 
-    var txt_Email = $('#defaultForm-email');
-    var showModel = $('#showModel');
-    var btnLogin = $('#btnlogin');
-    var txt_Password = $('#password');
-    btnLogin.click(function() {
-        var users = { 'Email': txt_Email.val(), 'Password': txt_Password.val() };
+    // SignUp
+    var txt_NameSignUp = $('#txt_NameSignUp');
+    var txt_EmailSignUp = $('#txt_EmailSignUp');
+    var txt_PasswordSignUp = $('#txt_PasswordSignUp');
+    var txt_FileUpload = $('#txt_FileUpload');
+    var btnSignUp = $('#btnSignUp');
+    var showModelSignUp = $('#showmodalSignup');
 
-        // Put the object into storage
-        localStorage.setItem('users', JSON.stringify(users));
-    })
-    showModel.click(function() {
-        var retrievedObject = localStorage.getItem('users');
-        // txt_Email.value = JSON.parse(retrievedObject.Email)
-        // txt_Password.value = JSON.parse(retrievedObject.Password)
-        console.log('retrievedObject: ', JSON.parse(retrievedObject))
+    // End SignUp
+    // json Array of User
+    var usersJson = [];
+    // json object of one User
+    var userObject;
+    // Login Status
+    var loginStatus = false;
+    showModelLogin.click(function() {
+        $('#myModalLogin').modal('show')
     });
-    // Retrieve the object from storage
+    btnLogin.click(function() {
+
+        if (txt_EmailLogin.val() == '') {
+            alert('Email is Required');
+        } else if (txt_PasswordLogin.val() == '') {
+            alert('Password is Required')
+        } else {
+            var Saved = JSON.parse(localStorage.getItem('users'));
+            if (Saved == null) {
+                toastr.error("Ypu Must Sign Up First ", "Opps !!");
+
+            } else {
+                for (var i = 0; i < Saved.length; i++) {
+                    loginStatus = false;
+                    // console.log(Saved[i].map(a => a.Email))
+                    if (txt_EmailLogin.val() == Saved[i].map(a => a.Email) &&
+                        txt_PasswordLogin.val() == Saved[i].map(a => a.Password)) {
+                        loginStatus = true;
+                        $('#myModalLogin').modal('hide');
+                        toastr.success("Login Successfully", "Done");
+
+                        break;
+                    }
+
+
+                }
+                console.log(loginStatus)
+            }
+            if (loginStatus == false) {
+                toastr.error("User Name Or Password InValid ", "Opps !!");
+                // alert("User Name Or Password InValid")
+            }
+
+        }
+
+    })
+
+    showModelSignUp.click(function() {
+        $('#myModalSignUp').modal('show')
+    });
+    btnSignUp.click(function() {
+        if (txt_NameSignUp.val() == '') {
+            alert('Name is Required');
+        } else if (txt_EmailSignUp.val() == '') {
+            alert('Email is Required')
+        } else if (txt_PasswordSignUp.val() == '') {
+            alert('Password is Required')
+        } else if (txt_FileUpload.val() == '') {
+            alert('Image is Required')
+        } else {
+            var Saved = JSON.parse(localStorage.getItem('users'));
+
+            if (Saved == null) {
+                userObject = [{
+                    'Id': usersJson.length + 1,
+                    'Name': txt_NameSignUp.val(),
+                    'Email': txt_EmailSignUp.val(),
+                    'Password': txt_PasswordSignUp.val(),
+                    'Image': txt_FileUpload.val()
+                }]
+                usersJson.push(userObject);
+                localStorage.setItem('users', JSON.stringify(usersJson));
+
+            } else {
+                userObject = [{
+                    'Id': Saved.length + 1,
+                    'Name': txt_NameSignUp.val(),
+                    'Email': txt_EmailSignUp.val(),
+                    'Password': txt_PasswordSignUp.val(),
+                    'Image': txt_FileUpload.val()
+                }]
+                Saved.push(userObject);
+                // Put the object into storage
+                localStorage.setItem('users', JSON.stringify(Saved));
+
+            }
+
+            $('#myModalSignUp').modal('hide');
+            toastr.success("Sign Up  Successfully", "Done");
+        }
+
+
+    })
 
 });
