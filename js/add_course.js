85b5e48ecba8ebc264af98c720d01cfd5090ed36
Ahmed Hamam <ahmedhamam50@gmail.com>
2019-05-04 00:59:43 +0200

    finsh add course

--- js/add_course.js
+++ js/add_course.js
@@ -25,6 +25,7 @@ var btn_save_qustion;
 var txt_course_title;
 var txt_num_videos;
 var txt_question_data;
+var div_questions;
 //=======================================================window load================================================
 window.onload = function() 
 {
@@ -34,7 +35,7 @@ window.onload = function()
     txt_course_title=document.getElementById("txt_course_title");
     txt_num_videos=document.getElementById("txt_num_videos");
     txt_question_data=document.querySelectorAll(".txt_question_data");
- 
+    div_questions=document.getElementById("div_questions");
     arr_courses = JSON.parse(localStorage.getItem('courses'));
     
     if(arr_courses==null)
@@ -352,6 +353,27 @@ window.onload = function()
 
     function btn_add_qusetion_click()
     {   
+//         div_questions.innerHtml+='<div class="col-md-10">'+
+//         '<div class="row">'+
+//         '<div class="col-md-12 col-sm-12 margin ">'+
+//             '<div class="text-left">'+
+//                 '<span class="question"> Question </span> <span class="delemeter">|</span>'+
+//                 '<span class="question-content">  ?.</span>'+
+//             '</div>'+
+//         '</div>'+
+//     '</div>'+
+//     '<div class="row m_l">'+
+//         '<div class="quiz" id="quiz" data-toggle="buttons">'+
+//             '<label class="  btn btn-lg btn-primary btn-block  answer0"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="radio" name="q_answer" value="1"></label>'+
+//             '<label class="  btn btn-lg btn-primary btn-block  answer0"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="radio" name="q_answer" value="2">2 Two</label>'+
+//             '<label class="  btn btn-lg btn-primary btn-block  answer0"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="radio" name="q_answer" value="3">3 Three</label>'+
+//             '<label class="  btn btn-lg btn-primary btn-block  answer0"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="radio" name="q_answer" value="4">4 Four</label>'+
+//         '</div>'+
+//     '</div>'+
+// '</div>';
+
+
+
             //  alert("btn_save_course_click")
     }
     function btn_save_course_click()
@@ -371,7 +393,10 @@ window.onload = function()
         course.videos=arr_course_videos;
         arr_courses.push(course);
         localStorage.setItem("courses", JSON.stringify(arr_courses));
-        
+        txt_course_title.value="";
+        txt_num_videos.value="";
+       // toastr.success("Course added Successfully", "Done");
+        document.open('https://www.codexworld.com', '_blank');
         // alert("btn_save_course_click")
     }
     function btn_save_qustion_click()
@@ -393,205 +418,29 @@ window.onload = function()
         }
         txt_question_data[5].value=1;
         $('#model_questions').modal('hide')
+        show_qustion_in_page(question);
+        toastr.success("question added Successfully", "Done");
+        
     }
+    function  show_qustion_in_page(q)
+    {
 
-
-
-
-//     // alert(arr_courses.length)
-//     if (arr_courses === null)
-//         arr_courses = [];
-//     alert(arr_courses.length)
-
-//     //alert(arr_courses[0].title)
-
-
-
-//     // var txt_question_choice=document.getElementsByClassName("question_choice");
-//     // //==============================================================================
-//     // //==============================================================================
-
-//     //==============================================================================    
-
-//     //==============================================================================
-
-
-
-//     //=========================this funcation to get the path of the video===================================
-
-//     var input_files = document.querySelector('input[type="file"]');
-//     input_files.onchange = previewFile;
-
-//     function previewFile() {
-//         arr_course_videos = [];
-   
-   
-   
-//    //=========================this funcation to get the path of the video===================================
-    
-//    var input_files=  document.querySelector('input[type="file"]');
-//     input_files.onchange=previewFile;
-//     function previewFile() 
-//     {       alert(input_files.value)
-//           arr_course_videos=[];
-//         //   alert(input_files.files.length)
-//         for (var i = 0; i < input_files.files.length; i++) {
-//             var file = input_files.files[i];
-//             arr_course_videos.push(file)
-//                 //  var reader  = new FileReader();
-//                 //  reader.onloadend = function ()
-//                 //  {
-//                 //     alert(reader.result)
-//                 //      arr_course_videos.push(reader.result)
-
-//             //  }
-//             //  if (file) 
-//             //  {
-//             //  reader.readAsDataURL(file);
-//             //  } 
-//         }
-//         //  alert(arr_course_videos)
-
-//     }
-
-//     //-----------------------------------------------
-
-//     // function btn_add_course_click()
-//     // {
-
-//     //     d= URL.createObjectURL(x.value); // set src to file url
-//     //     // var d=document.getElementById("add_vedios")
-//     //     var dd=document.getElementById("dd")
-//     //     alert(d)
-//     //     dd.innerHTML="<img src='"+d+"'>"
-
-//     // }
-//     // function btn_add_qusetion_click()
-//     // {
-//     //     for(var i=0;i<txt_question_choice.length;i++)
-//     //     {
-//     //         arr_qustion_choice.push(txt_question_choice[i].value);
-//     //     }
-//     //     qustion.choice=arr_qustion_choice;
-//     //     arr_qustion.push(qustion);
-//     //     course.questions=arr_qustion;
-//     //    
-//     // }
-//     // function btn_save_course_click()
-//     // {
-//     //     alert("btn_save_course_click")
-//     // }
-//     // function btn_save_qustion_click()
-//     // {
-//     //     alert("btn_save_qustion_click")
-//     // }
-//     //==============================================================================
-//     var btn_next = document.getElementById("next");
-//     btn_next.onclick = btn_next_Clik;
-// }
-// var qustion_number = 1;
-
-// function fun_add_question() {
-//     var div_questions = document.getElementById("div_qustions");
-//     div_questions.innerHTML += '<div id="div_qustion' + qustion_number + '">' +
-//         'Enter the question' + qustion_number + ' : <input type="text" class="question q' + qustion_number + '"><br><br>' +
-//         'Enter the question choice 1 : <input type="text" class="question_choice q' + qustion_number + '"><br><br>' +
-//         'Enter the question choice 2 : <input type="text" class="question_choice q' + qustion_number + '"><br><br>' +
-//         'Enter the question choice 3 : <input type="text" class="question_choice q' + qustion_number + '"><br><br>' +
-//         'Enter the question choice 4 : <input type="text" class="question_choice q' + qustion_number + '"><br><br>' +
-//         'Enter the question Answer : <input type="text"   class="answer q' + qustion_number + '"><br><br>' +
-//         '===============================================================================' +
-//         '</div>';
-//     qustion_number++;
-// }
-
-// function fun_save_course() {
-//     var txt_course_title = document.getElementById("course_title");
-//     course = new Object();
-//     course.id = arr_courses.length + 1;
-//     course.title = txt_course_title.value;
-//     course.videos = arr_course_videos;
-//     for (var i = 0; i < qustion_number - 1; i++) {
-//         var txt_question = document.querySelectorAll('.q' + (i + 1));
-//         var choice = [];
-//         question = new Object();
-//         question.id = i + 1;
-//         question.question_txt = txt_question[0].value;
-//         choice.push(txt_question[1].value);
-//         choice.push(txt_question[2].value);
-//         choice.push(txt_question[3].value);
-//         choice.push(txt_question[4].value);
-//         question.choice = choice;
-//         question.answer = txt_question[5].value;
-//         arr_qustion.push(question);
-//     }
-//     course.questions = arr_qustion;
-//     arr_courses.push(course);
-//     localStorage.setItem("courses", JSON.stringify(arr_courses));
-// }
-
-// <<<<<<< HEAD
-// var x = 0;
-
-// function btn_next_Clik() {
-//     alert(arr_courses[0].questions.length)
-//     alert(arr_courses[0].videos.length)
-
-// =======
-// var x=0;
-// function btn_next_Clik()
-// {   
-//     // alert(arr_courses[0].questions.length)
-//     // alert(arr_courses[0].videos.length)
- 
-// >>>>>>> 8847550a4bc700f0c8985d06299dbf09b27881b5
-
-//     // var preview = document.querySelector('video');
-
-
-//     if (x < arr_courses[0].videos.length) {
-//         var file = arr_courses[0].videos[x];
-//         x++;
-//         var reader = new FileReader();
-//         reader.onloadend = function() {
-//             preview.src = reader.result;
-//         }
-//         if (file) {
-//             reader.readAsDataURL(file);
-//         } else {
-//             preview.src = "";
-//         }
-//     } else {
-//         x = 0;
-//     }
-
-
-
-
-//     // if(x<arr_courses[0].videos.length)
-//     // {
-//     //     var file = arr_courses[0].videos[x];
-//     //     x++;
-//     //     var reader  = new FileReader();
-//     //     reader.onloadend = function ()
-//     //     {
-//     //     preview.src = reader.result;
-//     //     }
-//     //     if (file) 
-//     //     {
-//     //     reader.readAsDataURL(file);
-//     //     } 
-//     //     else 
-//     //     {
-//     //     preview.src = "";
-//     //     }
-//     // }
-//     // else
-//     // {
-//     //     x=0;
-//     // }
-  
-     
-    
-// >>>>>>> 8847550a4bc700f0c8985d06299dbf09b27881b5
-// }
+      var x='<div class="col-md-10"><div class="row">'+
+            '<div class="col-md-12 col-sm-12 margin ">'+
+                '<div class="text-left">'+
+                    '<span class="question"> Question '+q.id+'</span> <span class="delemeter">|</span>'+
+                    '<span class="question-content"> '+q.question+' ?.</span>'+
+                '</div>'+
+            '</div>'+
+        '</div>'+
+        '<div class="row m_l">'+
+            '<div class="quiz" id="quiz" data-toggle="buttons">'+
+                '<label class="  btn btn-lg btn-primary btn-block  answer0"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="radio" name="q_answer" value="1">'+q.choice[0]+'</label>'+
+                '<label class="  btn btn-lg btn-primary btn-block  answer0"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="radio" name="q_answer" value="2">'+q.choice[1]+'</label>'+
+                '<label class="  btn btn-lg btn-primary btn-block  answer0"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="radio" name="q_answer" value="3">'+q.choice[2]+'</label>'+
+                '<label class="  btn btn-lg btn-primary btn-block  answer0"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="radio" name="q_answer" value="4">'+q.choice[3]+'</label>'+
+            '</div>'+
+        '</div>'+
+    '</div>';
+    div_questions.innerHTML+=x;
+    }
